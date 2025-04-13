6989b4e69e78c0d151f3904d98f0f19b06a41041
blackzacktech <BArndt20@gmail.com>
2025-04-13 16:44:35 +0200

    ➕ Yurna AI Public

--- apps/bot/commands/General/help.ts
+++ apps/bot/commands/General/help.ts
@@ -1,229 +1,203 @@
 import { formatDuration } from "@yurna/util/functions/util";
-import { ApplicationCommandType, ApplicationCommandOptionType, EmbedBuilder, codeBlock, ActionRowBuilder, ButtonBuilder, ButtonStyle, StringSelectMenuBuilder, ComponentType, ApplicationIntegrationType, InteractionContextType } from "discord.js";
+import {
+  ApplicationCommandType,
+  ApplicationCommandOptionType,
+  EmbedBuilder,
+  codeBlock,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  StringSelectMenuBuilder,
+  ComponentType,
+  ApplicationIntegrationType,
+  InteractionContextType,
+} from "discord.js";
 import type { SlashCommand } from "@/util/types/Command";
 
 interface Category {
- name: string;
- emoji: string;
+  name: string;
+  emoji: string;
+}
+
+function getCategoryEmoji(category: string | undefined, emojis: Category[]): string {
+  const found = emojis.find((cat) => cat.name === category?.toLowerCase());
+  return found?.emoji ?? "❔";
 }
 
 export default {
- name: "help",
- description: "❔ Display a list of all available commands",
- type: ApplicationCommandType.ChatInput,
- cooldown: 5000,
- contexts: [InteractionContextType.Guild, InteractionContextType.BotDM, InteractionContextType.PrivateChannel],
- integrationTypes: [ApplicationIntegrationType.GuildInstall, ApplicationIntegrationType.UserInstall],
- usage: "/help [command]",
- options: [
-  {
-   name: "query",
-   description: "The full name of command or category",
-   autocomplete: true,
-   type: ApplicationCommandOptionType.String,
-   max_length: 256,
-   required: false,
-  },
- ],
- autocomplete: async (client, interaction) => {
-  const focusedOption = interaction.options.getFocused(true);
-  if (focusedOption.name === "query") {
-   const commands = focusedOption.value // prettier
-    ? Array.from(client.slashCommands.filter((cmd) => cmd.name.toLowerCase().includes(focusedOption.value.toLowerCase())).values())
-    : Array.from(client.slashCommands.values());
+  name: "help",
+  description: "❔ Display a list of all available commands",
+  type: ApplicationCommandType.ChatInput,
+  cooldown: 5000,
+  contexts: [InteractionContextType.Guild, InteractionContextType.BotDM, InteractionContextType.PrivateChannel],
+  integrationTypes: [ApplicationIntegrationType.GuildInstall, ApplicationIntegrationType.UserInstall],
+  usage: "/help [command]",
+  options: [
+    {
+      name: "query",
+      description: "The full name of command or category",
+      autocomplete: true,
+      type: ApplicationCommandOptionType.String,
+      max_length: 256,
+      required: false,
+    },
+  ],
+  autocomplete: async (client, interaction) => {
+    const focusedOption = interaction.options.getFocused(true);
+    if (focusedOption.name === "query") {
+      const commands = focusedOption.value
+        ? Array.from(client.slashCommands.filter((cmd) => cmd.name.toLowerCase().includes(focusedOption.value.toLowerCase())).values())
+        : Array.from(client.slashCommands.values());
 
-   await interaction.respond(
-    // prettier
-    commands.slice(0, 25).map((choice) => ({
-     name: `/${choice.name} - ${choice.description}`,
-     value: choice.name,
-    }))
-   );
-  }
- },
- run: async (client, interaction, guildSettings) => {
-  try {
-   if (!client.user) return client.errorMessages.createSlashError(interaction, "❌ Bot is not ready yet. Please try again later.");
-
-   const globalActionRow: ActionRowBuilder<ButtonBuilder | StringSelectMenuBuilder>[] = [];
-   const inviteLink = `https://discord.com/oauth2/authorize/?permissions=${client.config.permissions}&scope=${client.config.scopes}&client_id=${client.user.id}`;
-
-   if (client.config.url) {
-    const buttonRow = new ActionRowBuilder<ButtonBuilder>() // prettier
-     .addComponents(
-      new ButtonBuilder() // prettier
-       .setStyle(ButtonStyle.Link)
-       .setLabel("Dashboard")
-       .setURL(client.config.url),
-      new ButtonBuilder() // prettier
-       .setStyle(ButtonStyle.Link)
-       .setLabel("Invite")
-       .setURL(inviteLink),
-      new ButtonBuilder() // prettier
-       .setStyle(ButtonStyle.Link)
-       .setLabel("All Commands")
-       .setURL(`${client.config.url}/commands`)
-     );
-    globalActionRow.push(buttonRow);
-   }
-
-   const query = interaction.options.getString("query") || "";
-   const isCategory = client.slashCommands.map((cmd) => cmd.category?.toLowerCase()).includes(query?.toLowerCase());
-
-   // #region Command
-   if (query && !isCategory) {
-    // If the query is a command, display the command's help menu.
-    const command = client.slashCommands.get(query.toLowerCase());
-
-    if (!command) {
-     return client.errorMessages.createSlashError(interaction, `❌ The command \`${query}\` does not exist. Please check your spelling and try again.`);
-    }
-
-    const embed = new EmbedBuilder()
-     .setTitle(`❔ Help for /${command.name}`)
-     .addFields([
-      {
-       name: "Name",
-       value: codeBlock(command.name),
-       inline: true,
-      },
-      {
-       name: "Usage",
-       value: codeBlock(command.usage),
-       inline: true,
-      },
-      {
-       name: "Description",
-       value: codeBlock(command.description),
-      },
-      {
-       name: "Cooldown",
-       value: codeBlock(formatDuration(command.cooldown || 0)),
-       inline: true,
-      },
-      {
-       name: "Category",
-       value: codeBlock(command.category || "General"),
-       inline: true,
-      },
-     ])
-     .setColor(guildSettings?.embedColor || client.config.defaultColor)
-     .setTimestamp()
-     .setFooter({
-      text: `Requested by ${interaction.user.globalName || interaction.user.username}`,
-      iconURL: interaction.user.displayAvatarURL({
-       size: 256,
-      }),
-     });
-
-    return interaction.followUp({ ephemeral: false, embeds: [embed], components: globalActionRow });
-    // #endregion
-    // #region Category
-   } else if (query && isCategory) {
-    // If the query is a category, display all commands in that category.
-    const commands = client.slashCommands.filter((cmd) => cmd.category?.toLowerCase() === query.toLowerCase());
-    const embed = new EmbedBuilder()
-     .setTitle(`${client.config.emojis.categories.find((cat: Category) => cat.name === query.toLowerCase()).emoji} Available \`${query}\` commands \`(${commands.size})\``)
-     .setDescription(`> ${commands.map((cmd) => `\`/${cmd.name}\``).join(", ")}`)
-     .setColor(guildSettings?.embedColor || client.config.defaultColor)
-     .setTimestamp()
-     .setFooter({
-      text: `Requested by ${interaction.user.globalName || interaction.user.username}`,
-      iconURL: interaction.user.displayAvatarURL({
-       size: 256,
-      }),
-     });
-    return interaction.followUp({ ephemeral: false, embeds: [embed], components: globalActionRow });
-    // #endregion
-    // #region Main menu
-   } else {
-    // If there is no query, display the main help menu.
-    const categories = [...new Set(client.slashCommands.map((cmd) => cmd.category))];
-
-    const embed = new EmbedBuilder()
-     .setTitle("❔ Help")
-     .setDescription(`> Use the menu, or use [\`/help [category]\`](${inviteLink}) to view commands based on their category!`)
-     .addFields(
-      categories
-       .map((category) => ({
-        name: `${client.config.emojis.categories.find((cat: Category) => cat.name === category?.toLowerCase()).emoji} ${category}`,
-        value: codeBlock(`/help ${category?.toLowerCase()}`),
-        inline: `/help ${category?.toLowerCase()}`.length < 15,
-       }))
-       .sort((a, b) => (a.inline === b.inline ? a.name.length - b.name.length : a.inline ? -1 : 1))
-     )
-     .setColor(guildSettings?.embedColor || client.config.defaultColor)
-     .setTimestamp()
-     .setThumbnail(client.user.displayAvatarURL({ size: 256 }))
-     .setAuthor({
-      name: `${client.user.username} Help`,
-      iconURL: client.user.displayAvatarURL({ size: 256 }),
-     })
-     .setFooter({
-      text: `Requested by ${interaction.user.globalName || interaction.user.username}`,
-      iconURL: interaction.user.displayAvatarURL({
-       size: 256,
-      }),
-     });
-
-    const selectRow = new ActionRowBuilder<StringSelectMenuBuilder>() // prettier
-     .addComponents(
-      new StringSelectMenuBuilder() // prettier
-       .setCustomId("help_select")
-       .setPlaceholder("Select a category")
-       .addOptions(
-        categories.map((category) => ({
-         label: `${client.config.emojis.categories.find((cat: Category) => cat.name === category?.toLowerCase()).emoji} ${category}`,
-         description: `View all ${client.slashCommands.filter((cmd) => cmd.category?.toLowerCase() === category?.toLowerCase()).size} commands`,
-         value: category?.toLowerCase() ?? "",
+      await interaction.respond(
+        commands.slice(0, 25).map((choice) => ({
+          name: `/${choice.name} - ${choice.description}`,
+          value: choice.name,
         }))
-       )
-     );
-    const actionRow = [selectRow, ...globalActionRow];
+      );
+    }
+  },
+  run: async (client, interaction, guildSettings) => {
+    try {
+      if (!client.user) return client.errorMessages.createSlashError(interaction, "❌ Bot is not ready yet. Please try again later.");
 
-    const response = await interaction.followUp({ ephemeral: false, embeds: [embed], components: actionRow });
+      const globalActionRow: ActionRowBuilder<ButtonBuilder | StringSelectMenuBuilder>[] = [];
+      const inviteLink = `https://discord.com/oauth2/authorize/?permissions=${client.config.permissions}&scope=${client.config.scopes}&client_id=${client.user.id}`;
 
-    const collector = response.createMessageComponentCollector({
-     componentType: ComponentType.StringSelect,
-     filter: (i) => i.user.id === interaction.user.id,
-     time: 3 * 60 * 1000, // 30 seconds
-    });
+      if (client.config.url) {
+        const buttonRow = new ActionRowBuilder<ButtonBuilder>().addComponents(
+          new ButtonBuilder().setStyle(ButtonStyle.Link).setLabel("Dashboard").setURL(client.config.url),
+          new ButtonBuilder().setStyle(ButtonStyle.Link).setLabel("Invite").setURL(inviteLink),
+          new ButtonBuilder().setStyle(ButtonStyle.Link).setLabel("All Commands").setURL(`${client.config.url}/commands`)
+        );
+        globalActionRow.push(buttonRow);
+      }
 
-    collector.on("collect", async (i) => {
-     /* eslint-disable-next-line prefer-destructuring */
-     const category = i.values[0];
-     const commands = client.slashCommands.filter((cmd) => cmd.category?.toLowerCase() === (category ?? "").toLowerCase());
-     const embed = new EmbedBuilder()
-      .setTitle(`${client.config.emojis.categories.find((cat: Category) => cat.name === (category ?? "").toLowerCase()).emoji} Available \`${category ?? ""}\` commands \`(${commands.size})\``)
-      .setDescription(`> ${commands.map((cmd) => `\`/${cmd.name}\``).join(", ")}`)
-      .setColor(guildSettings?.embedColor || client.config.defaultColor)
-      .setTimestamp()
-      .setFooter({
-       text: `Requested by ${interaction.user.globalName || interaction.user.username}`,
-       iconURL: interaction.user.displayAvatarURL({
-        size: 256,
-       }),
-      });
-     try {
-      await i.update({ embeds: [embed], components: actionRow });
-     } catch (_err) {
-      return;
-     }
-    });
+      const query = interaction.options.getString("query") || "";
+      const emojis = client.config.emojis.categories as Category[];
+      const isCategory = client.slashCommands.map((cmd) => cmd.category?.toLowerCase()).includes(query?.toLowerCase());
 
-    collector.on("end", async () => {
-     try {
-      await interaction.editReply({ embeds: [embed], components: globalActionRow });
-     } catch (_err) {
-      return;
-     }
-    });
+      if (query && !isCategory) {
+        const command = client.slashCommands.get(query.toLowerCase());
+        if (!command) {
+          return client.errorMessages.createSlashError(interaction, `❌ The command \`${query}\` does not exist. Please check your spelling and try again.`);
+        }
 
-    return;
-   }
-   // #endregion
-  } catch (err) {
-   client.errorMessages.internalError(interaction, err);
-  }
- },
+        const embed = new EmbedBuilder()
+          .setTitle(`❔ Help for /${command.name}`)
+          .addFields([
+            { name: "Name", value: codeBlock(command.name), inline: true },
+            { name: "Usage", value: codeBlock(command.usage), inline: true },
+            { name: "Description", value: codeBlock(command.description) },
+            { name: "Cooldown", value: codeBlock(formatDuration(command.cooldown || 0)), inline: true },
+            { name: "Category", value: codeBlock(command.category || "General"), inline: true },
+          ])
+          .setColor(guildSettings?.embedColor || client.config.defaultColor)
+          .setTimestamp()
+          .setFooter({
+            text: `Requested by ${interaction.user.globalName || interaction.user.username}`,
+            iconURL: interaction.user.displayAvatarURL({ size: 256 }),
+          });
+
+        return interaction.followUp({ ephemeral: false, embeds: [embed], components: globalActionRow });
+      } else if (query && isCategory) {
+        const commands = client.slashCommands.filter((cmd) => cmd.category?.toLowerCase() === query.toLowerCase());
+
+        const emoji = getCategoryEmoji(query, emojis);
+        const embed = new EmbedBuilder()
+          .setTitle(`${emoji} Available \`${query}\` commands \`(${commands.size})\``)
+          .setDescription(`> ${commands.map((cmd) => `\`/${cmd.name}\``).join(", ")}`)
+          .setColor(guildSettings?.embedColor || client.config.defaultColor)
+          .setTimestamp()
+          .setFooter({
+            text: `Requested by ${interaction.user.globalName || interaction.user.username}`,
+            iconURL: interaction.user.displayAvatarURL({ size: 256 }),
+          });
+
+        return interaction.followUp({ ephemeral: false, embeds: [embed], components: globalActionRow });
+      } else {
+        const categories = [...new Set(client.slashCommands.map((cmd) => cmd.category))];
+
+        const embed = new EmbedBuilder()
+          .setTitle("❔ Help")
+          .setDescription(`> Use the menu, or use [\`/help [category]\`](${inviteLink}) to view commands based on their category!`)
+          .addFields(
+            categories
+              .map((category) => ({
+                name: `${getCategoryEmoji(category, emojis)} ${category ?? "General"}`,
+                value: codeBlock(`/help ${category?.toLowerCase()}`),
+                inline: `/help ${category?.toLowerCase()}`.length < 15,
+              }))
+              .sort((a, b) => (a.inline === b.inline ? a.name.length - b.name.length : a.inline ? -1 : 1))
+          )
+          .setColor(guildSettings?.embedColor || client.config.defaultColor)
+          .setTimestamp()
+          .setThumbnail(client.user.displayAvatarURL({ size: 256 }))
+          .setAuthor({
+            name: `${client.user.username} Help`,
+            iconURL: client.user.displayAvatarURL({ size: 256 }),
+          })
+          .setFooter({
+            text: `Requested by ${interaction.user.globalName || interaction.user.username}`,
+            iconURL: interaction.user.displayAvatarURL({ size: 256 }),
+          });
+
+        const selectRow = new ActionRowBuilder<StringSelectMenuBuilder>().addComponents(
+          new StringSelectMenuBuilder()
+            .setCustomId("help_select")
+            .setPlaceholder("Select a category")
+            .addOptions(
+              categories.map((category) => ({
+                label: `${getCategoryEmoji(category, emojis)} ${category ?? "General"}`,
+                description: `View all ${client.slashCommands.filter((cmd) => cmd.category?.toLowerCase() === category?.toLowerCase()).size} commands`,
+                value: category?.toLowerCase() ?? "",
+              }))
+            )
+        );
+
+        const actionRow = [selectRow, ...globalActionRow];
+        const response = await interaction.followUp({ ephemeral: false, embeds: [embed], components: actionRow });
+
+        const collector = response.createMessageComponentCollector({
+          componentType: ComponentType.StringSelect,
+          filter: (i) => i.user.id === interaction.user.id,
+          time: 3 * 60 * 1000,
+        });
+
+        collector.on("collect", async (i) => {
+          const selectedCategory = i.values[0];
+          const commands = client.slashCommands.filter((cmd) => cmd.category?.toLowerCase() === selectedCategory);
+
+          const emoji = getCategoryEmoji(selectedCategory, emojis);
+          const embed = new EmbedBuilder()
+            .setTitle(`${emoji} Available \`${selectedCategory}\` commands \`(${commands.size})\``)
+            .setDescription(`> ${commands.map((cmd) => `\`/${cmd.name}\``).join(", ")}`)
+            .setColor(guildSettings?.embedColor || client.config.defaultColor)
+            .setTimestamp()
+            .setFooter({
+              text: `Requested by ${interaction.user.globalName || interaction.user.username}`,
+              iconURL: interaction.user.displayAvatarURL({ size: 256 }),
+            });
+
+          try {
+            await i.update({ embeds: [embed], components: actionRow });
+          } catch {
+            return;
+          }
+        });
+
+        collector.on("end", async () => {
+          try {
+            await interaction.editReply({ embeds: [embed], components: globalActionRow });
+          } catch {
+            return;
+          }
+        });
+
+        return;
+      }
+    } catch (err) {
+      client.errorMessages.internalError(interaction, err);
+    }
+  },
 } satisfies SlashCommand;
--- apps/bot/index.ts
+++ apps/bot/index.ts
@@ -22,53 +22,53 @@ Logger("info", "Starting Yurna.info Bot...");
 Logger("info", `Running version v${process.env.npm_package_version} on Node.js ${process.version} on ${process.platform} ${process.arch}`);
 
 class Yurnabot extends Client {
- // eslint-disable-next-line typescript/no-explicit-any
- public config: Record<string, any> = {};
- public modals: Collection<string, Modal> = new Collection();
- public slashCommands: Collection<string, SlashCommand> = new Collection();
- public additionalSlashCommands: number = 0;
- public commandsRan: number = 0;
- public giveawaysManager: GiveawaysManager = {} as GiveawaysManager;
- public errorMessages: {
-  internalError: (interaction: CommandInteraction, error: unknown) => Promise<void>;
-  createSlashError: (interaction: CommandInteraction, description: string, title?: string) => void;
- } = {
-  internalError: async (interaction: CommandInteraction, error: unknown): Promise<void> => {
-   Logger("error", error?.toString() ?? "Unknown error occurred");
-   const embed = createErrorEmbed("An error occurred while executing this command. Please try again later.", "Unknown error occurred");
-   await interaction.followUp({ embeds: [embed], ephemeral: true });
-  },
-  createSlashError: (interaction: CommandInteraction, description: string, title?: string): void => {
-   const embed = createErrorEmbed(description, title);
-   embed.setFooter({
-    text: `Requested by ${interaction.user.globalName ?? interaction.user.username}`,
-    iconURL: interaction.user.displayAvatarURL(),
-   });
-   interaction.followUp({ embeds: [embed], ephemeral: true });
-  },
- };
- public debugger: (type: "info" | "event" | "error" | "warn" | "ready" | "cron", message: string | unknown) => void = (type, message) => {
-  Logger(type, message);
- };
- public performance: (time: number) => string = (time: number): string => {
-  const run = Math.floor(performance.now() - time);
-  return run > 500 ? chalk.underline.red(`${run}ms`) : chalk.underline(`${run}ms`);
- };
+    // eslint-disable-next-line typescript/no-explicit-any
+    public config: Record<string, any> = {};
+    public modals: Collection<string, Modal> = new Collection();
+    public slashCommands: Collection<string, SlashCommand> = new Collection();
+    public additionalSlashCommands: number = 0;
+    public commandsRan: number = 0;
+    public giveawaysManager: GiveawaysManager = {} as GiveawaysManager;
+    public errorMessages: {
+        internalError: (interaction: CommandInteraction, error: unknown) => Promise<void>;
+        createSlashError: (interaction: CommandInteraction, description: string, title?: string) => void;
+    } = {
+            internalError: async (interaction: CommandInteraction, error: unknown): Promise<void> => {
+                Logger("error", error?.toString() ?? "Unknown error occurred");
+                const embed = createErrorEmbed("An error occurred while executing this command. Please try again later.", "Unknown error occurred");
+                await interaction.followUp({ embeds: [embed], ephemeral: true });
+            },
+            createSlashError: (interaction: CommandInteraction, description: string, title?: string): void => {
+                const embed = createErrorEmbed(description, title);
+                embed.setFooter({
+                    text: `Requested by ${interaction.user.globalName ?? interaction.user.username}`,
+                    iconURL: interaction.user.displayAvatarURL(),
+                });
+                interaction.followUp({ embeds: [embed], ephemeral: true });
+            },
+        };
+    public debugger: (type: "info" | "event" | "error" | "warn" | "ready" | "cron", message: string | unknown) => void = (type, message) => {
+        Logger(type, message);
+    };
+    public performance: (time: number) => string = (time: number): string => {
+        const run = Math.floor(performance.now() - time);
+        return run > 500 ? chalk.underline.red(`${run}ms`) : chalk.underline(`${run}ms`);
+    };
 }
 
 const client = new Yurnabot({
- intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildModeration, GatewayIntentBits.GuildEmojisAndStickers, GatewayIntentBits.GuildIntegrations, GatewayIntentBits.GuildWebhooks, GatewayIntentBits.GuildInvites, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildPresences, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.GuildMessageTyping, GatewayIntentBits.DirectMessages, GatewayIntentBits.DirectMessageReactions, GatewayIntentBits.DirectMessageTyping, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildScheduledEvents, GatewayIntentBits.AutoModerationConfiguration, GatewayIntentBits.AutoModerationExecution, GatewayIntentBits.GuildMessagePolls, GatewayIntentBits.DirectMessagePolls],
- partials: [Partials.Message, Partials.Reaction, Partials.User],
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildModeration, GatewayIntentBits.GuildEmojisAndStickers, GatewayIntentBits.GuildIntegrations, GatewayIntentBits.GuildWebhooks, GatewayIntentBits.GuildInvites, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildPresences, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.GuildMessageTyping, GatewayIntentBits.DirectMessages, GatewayIntentBits.DirectMessageReactions, GatewayIntentBits.DirectMessageTyping, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildScheduledEvents, GatewayIntentBits.AutoModerationConfiguration, GatewayIntentBits.AutoModerationExecution, GatewayIntentBits.GuildMessagePolls, GatewayIntentBits.DirectMessagePolls],
+    partials: [Partials.Message, Partials.Reaction, Partials.User, Partials.Channel],
 });
 
 client.config = {
- ...botConfig,
- ...globalPermissions,
- ...globalConfig,
- ...debuggerConfig,
- ...dashboardConfig,
- ...reactionRoleConfig,
- logChannelId: "1351286961700081865",
+    ...botConfig,
+    ...globalPermissions,
+    ...globalConfig,
+    ...debuggerConfig,
+    ...dashboardConfig,
+    ...reactionRoleConfig,
+    logChannelId: "1351286961700081865",
 };
 
 await loadCommands(client);
@@ -82,19 +82,19 @@ client.giveawaysManager = giveaway(client) as unknown as GiveawaysManager;
 Logger("info", "Logging in...");
 
 client.on("debug", (invite) => {
- if (client.config.displayDebugMessages) Logger("info", invite);
+    if (client.config.displayDebugMessages) Logger("info", invite);
 });
 
 process.on("unhandledRejection", (reason) => {
- Logger("error", `Unhandled rejection: ${reason}`);
+    Logger("error", `Unhandled rejection: ${reason}`);
 });
 
 process.on("uncaughtException", (error) => {
- Logger("error", `Uncaught exception: ${error}`);
+    Logger("error", `Uncaught exception: ${error}`);
 });
 
 process.on("warning", (warning) => {
- Logger("warn", `Warning: ${warning}`);
+    Logger("warn", `Warning: ${warning}`);
 });
 
 await client.login(process.env.TOKEN);
